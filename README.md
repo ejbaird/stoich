4722c676afdd7076d9ae46f78c899f8e6244aa9a
Elizabeth Baird <eliz.baird@gmail.com>
2014-04-25 15:25:17 -0400

    continuing

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ stoich
 ======
 
 CTurner's project for determining counts and ratios of elements in amino acids in E. coli genome
+
+Type the following into the command line:
+
+    python elstoich.py -r <name of reference file> -d <name of differential expression file>
+
+For example, 
+
+    python elstoich.py -r REL606.2.gbk -d arrays.txt
--- elstoich.py
+++ elstoich.py
@@ -4,52 +4,48 @@
 
 import argparse
 import os 
-import sys
+import sys, getopt
 import fileinput
 
-import config
-
 from Bio import SeqIO
-from Bio.seq import Seq
+from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
-from bumpy import random
-import numpy as np
-import operator
-import random
-import pandas as pd
-
-def readinput():
-    for line in file.input():
-        process(line)
-    return stuff?
+def parse_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', dest='reference')
+    parser.add_argument('-d', dest='diff_exp')
+    args = parser.parse_args
+    #print reference, diff_exp
+    return args
 
 def AAelements():
     aa = {}
-    aa.A = {3let='Ala', C=3, H=7, N=1, O=2}
-    aa.R = {3let='Arg', C=6, H=14, N=4, O=2}
-    aa.N = {3let='Asn', C=4, H=8, N=2, O=3}
-    aa.D = {3let='Asp', C=7, H=7, N=1, O=4}
-    aa.C = {3let='Cys', C=3, H=7, N=1, O=2, S=1}
-    aa.Q = {3let='Gln', C=5, H=10, N=2, O=3}
-    aa.E = {3let='Glu', C=5, H=9, N=1, O=4}
-    aa.G = {3let='Gly', C=2, H=5, N=1, O=2}
-    aa.H = {3let='His', C=6, H=9, N=3, O=2}
-    aa.I = {3let='Ile', C=6, H=13, N=1, O=2}
-    aa.L = {3let='Leu', C=6, H=13, N=1, O=2}
-    aa.K = {3let='Lys', C=6, H=14, N=2, O=2}
-    aa.M = {3let='Met', C=5, H=11, N=1, O=2, S=1}
-    aa.F = {3let='Phe', C=9, H=11, N=1, O=2}
-    aa.P = {3let='Pro', C=5, H=9, N=1, O=2}
-    aa.S = {3let='Ser', C=3, H=7, N=1, O=3}
-    aa.T = {3let='Thr', C=4, H=9, N=1, O=3}
-    aa.W = {3let='Trp', C=11, H=12, N=2, O=2}
-    aa.Y = {3let='Tyr', C=9, H=11, N=1, O=3}
-    aa.V = {3let='Val', C=5, H=11, N=1, O=2}
+    aa.A = {abbrev:'Ala', C:3, H:7, N:1, O:2} #aa.A or aa[A] ?
+    aa.R = {abbrev:'Arg', C:6, H:14, N:4, O:2}
+    aa.N = {abbrev:'Asn', C:4, H:8, N:2, O:3}
+    aa.D = {abbrev:'Asp', C:7, H:7, N:1, O:4}
+    aa.C = {abbrev:'Cys', C:3, H:7, N:1, O:2, S:1}
+    aa.Q = {abbrev:'Gln', C:5, H:10, N:2, O:3}
+    aa.E = {abbrev:'Glu', C:5, H:9, N:1, O:4}
+    aa.G = {abbrev:'Gly', C:2, H:5, N:1, O:2}
+    aa.H = {abbrev:'His', C:6, H:9, N:3, O:2}
+    aa.I = {abbrev:'Ile', C:6, H:13, N:1, O:2}
+    aa.L = {abbrev:'Leu', C:6, H:13, N:1, O:2}
+    aa.K = {abbrev:'Lys', C:6, H:14, N:2, O:2}
+    aa.M = {abbrev:'Met', C:5, H:11, N:1, O:2, S:1}
+    aa.F = {abbrev:'Phe', C:9, H:11, N:1, O:2}
+    aa.P = {abbrev:'Pro', C:5, H:9, N:1, O:2}
+    aa.S = {abbrev:'Ser', C:3, H:7, N:1, O:3}
+    aa.T = {abbrev:'Thr', C:4, H:9, N:1, O:3}
+    aa.W = {abbrev:'Trp', C:11, H:12, N:2, O:2}
+    aa.Y = {abbrev:'Tyr', C:9, H:11, N:1, O:3}
+    aa.V = {abbrev:'Val', C:5, H:11, N:1, O:2}
     return aa
 
+def main():
+    args = parse_input()
+    AA = AAelements() 
 
-
-
-
+main()
